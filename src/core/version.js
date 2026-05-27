fc2c97bd4d29afafc3f00a4189eca8450c38ad64
Xuanlu Dev <xuanlu-dev@example.invalid>
2026-05-27 13:57:58 +0800

    v0.3.19：shellReflect实测正常+荆棘甲5block防上位替代

--- src/core/version.js
+++ src/core/version.js
@@ -1,8 +1,8 @@
 export const gameVersion = {
-  app: "0.3.18",
-  core: "0.3.18",
+  app: "0.3.19",
+  core: "0.3.19",
   ui: "0.1.0",
   data: "0.1.0",
-  save: "0.3.18",
-  label: "v0.3.18_core-0.3.18_ui-0.1.0_data-0.1.0",
+  save: "0.3.19",
+  label: "v0.3.19_core-0.3.19_ui-0.1.0_data-0.1.0",
 };
