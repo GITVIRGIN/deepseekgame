b131f94d98d52a0cb7029a1c4b9295f5034eee43
Xuanlu Dev <xuanlu-dev@example.invalid>
2026-05-27 12:12:11 +0800

    v0.3.10：血条放大14px+目标缩小8px

--- src/core/version.js
+++ src/core/version.js
@@ -1,8 +1,8 @@
 export const gameVersion = {
-  app: "0.3.9",
-  core: "0.3.9",
+  app: "0.3.10",
+  core: "0.3.10",
   ui: "0.1.0",
   data: "0.1.0",
-  save: "0.3.9",
-  label: "v0.3.9_core-0.3.9_ui-0.1.0_data-0.1.0",
+  save: "0.3.10",
+  label: "v0.3.10_core-0.3.10_ui-0.1.0_data-0.1.0",
 };
